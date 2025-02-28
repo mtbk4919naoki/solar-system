60b652367efc3cebf179cde219f21cbea73b15bf
Naoki Yamamoto <mtbk4919naoki@gmail.com>
2025-02-28 22:34:21 +0900

    chore: github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://mtbk4919naoki.github.io/solar-system/',
+  base: '/solar-system/',
+});
